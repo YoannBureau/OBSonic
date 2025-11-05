78e437b93fedc172bf30014eba89ae0955b8a0b3
Yoann Bureau <ybureau@gmail.com>
2025-11-05 11:15:03 +0100

    refactor: switch Monaco Editor to local files and improve editor styles

--- public/editor/editor.js
+++ public/editor/editor.js
@@ -18,7 +18,7 @@ class PlayerEditor {
     initMonaco() {
         require.config({ 
             paths: { 
-                'vs': 'https://cdn.jsdelivr.net/npm/monaco-editor@0.45.0/min/vs' 
+                'vs': '/libs/monaco-editor/vs' 
             }
         });
 
